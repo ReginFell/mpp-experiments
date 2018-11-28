67234c3b2a35484ee46c1f9017f19af587921444
ReginFell <ReginFell@gmail.com>
2018-11-28 23:12:39 +0200

    move to multiplatform-module

--- app/src/main/java/com/regin/additivefood/application/di/DatabaseModule.kt
+++ app/src/main/java/com/regin/additivefood/application/di/DatabaseModule.kt
@@ -1,4 +1,10 @@
 package com.regin.additivefood.application.di
 
 import android.content.Context
+import com.regin.additivefood.navigation.AppRouter
 import com.squareup.sqldelight.android.AndroidSqlDatabase
+import org.koin.dsl.module.module
+
+val databaseModule = module {
+
+}
