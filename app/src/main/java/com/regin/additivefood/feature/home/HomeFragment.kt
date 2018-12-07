e63d46de6621b526e5cfc43a8b820ba72618ce49
ReginFell <ReginFell@gmail.com>
2018-12-07 19:50:36 +0200

    Add toolbar

--- app/src/main/java/com/regin/additivefood/feature/home/HomeFragment.kt
+++ app/src/main/java/com/regin/additivefood/feature/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.regin.additivefood.R
@@ -28,6 +29,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
         list.adapter = adapter
 
         homeViewModel.additiveLiveData.observe(this, Observer { adapter.submitList(it)})
